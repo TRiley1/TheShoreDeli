aeb597dfbbb6c55d08ed0c37d79d227bb5bb113e
TRiley1 <thomas.edward.riley@gmail.com>
2023-09-03 16:18:44 +0100

    added more menu options

--- src/components/menu/TheShoreMenu.js
+++ src/components/menu/TheShoreMenu.js
@@ -16,7 +16,10 @@ class Item {
   const sandwich1 = createItem("Sandwich", "Big P", "£7.00", "Oak smoked cheddar, Honey roast ham, Coleslaw, Mustard, Pickles & Rocket");
   const sandwich2 = createItem("Sandwich", "Mummas", "£7.50", "Basil, Pesto, Mozzarella, Tomatoes, Prosciutto, Salami & Rocket");
   const sandwich3 = createItem("Sandwich", "Georgia", "£7.50", "Goat's cheese, Prosciutto, Hazelnuts, Honey & Rocket")
-  
+  const sandwich4 = createItem("Sandwich", "Life of Bryan", "£7.00", "Tomato Sugo, Mozzarella, Roast Peppers, Spainich & Chili Oil")
+  const sandwich5 = createItem("Sandwich", "The Niall", "£7.50", "Aioli, Gruyere, Honey Roast Ham, Rocket & Caramelised onions")
+  const sandwich6 = createItem("Sandwich", "Cousin Rachael", "£6.50", "Red Pesto, Falafel, Aubergine, Roast Peppers & Spainch")
+
   export const drinksMenu = [drink1, drink2];
-  export const sandwichMenu = [sandwich1, sandwich2, sandwich3];
+  export const sandwichMenu = [sandwich1, sandwich2, sandwich3,sandwich4, sandwich5, sandwich6];
   
